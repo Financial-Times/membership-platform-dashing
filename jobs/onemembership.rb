bb1fc948c53633e0a193959f778be12052ac5189
Mark Lacdao <mark.lacdao@gmail.com>
2016-10-05 16:52:44 +0800

    Added User Profile Service AT monitor for US

--- jobs/onemembership.rb
+++ jobs/onemembership.rb
@@ -163,6 +163,7 @@ end
 SCHEDULER.every '30s', first_in: 0 do |job|
 
   performCheckAndSendEventToWidgets('user-profile', 'user-profile-svc-at-lb-eu-west-1.memb.ft.com', '/tests/get-profile-critical', true)
+  performCheckAndSendEventToWidgets('user-profile-us', 'user-profile-svc-at-lb-us-east-1.memb.ft.com', '/tests/get-profile-critical', true)
   getStatusFromHealthCheck('user-profile-eu', 'http://healthcheck.ft.com', '/service/7b02faa0e45544c26c7f4dddcdafa251', s3oCredentials)
   getStatusFromHealthCheck('user-profile-us', 'http://healthcheck.ft.com', '/service/41ddaf5f7110db4f05cf1104d21c0d78', s3oCredentials)
   getStatusFromHealthCheck('sign-up-app-us', 'http://healthcheck.ft.com', '/service/f3b5a9c8792abcb2ec22000768719f4f', s3oCredentials)
